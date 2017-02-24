e5e06b1108dec21cd78e98662e6a7e4573f627dc
ilarjod <ilari.hotti@gmail.com>
2017-02-24 22:48:48 +0200

    datawrangling and chapter 5
    datawrangling and chapter 5

--- data/create_human.R
+++ data/create_human.R
@@ -1,5 +1,7 @@
 library(tidyverse)
-
+library(dplyr)
+library(stringr)
+library(GGally)
 #read data to rstudio from web
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
@@ -11,15 +13,13 @@ summary(hd)
 summary(gii)
 
 #rename columns
-colnames(hd) <- c("HDI_rank", "country", "HDI", "life_exp", "edu_exp", "edu_mean", "GNI/capita", "GNIrank-HDIrank")
+colnames(hd) <- c("HDI_rank", "country", "HDI", "life_exp", "edu_exp", "edu_mean", "GNI.capita", "GNIrank-HDIrank")
 
-colnames(gii) <- c("GII_rank", "country", "GII", "mat_mort", "adol_br", "parl_rep", "edu_2nd_f", "edu_2nd_m", "lab_f", "lab_m")
+colnames(gii) <- c("GII_rank", "country", "GII", "mat_mort", "adol_birth", "parl_rep_f", "edu_2nd_f", "edu_2nd_m", "lab_f", "lab_m")
 
 #check the results
 str(hd)
 str(gii)
-
-
 
 #create two new variables
 gii <-  mutate(gii, edu_2nd_ratio = edu_2nd_f/edu_2nd_m)
@@ -34,3 +34,34 @@ str(human)
 
 #save the data to file
 write.table(human, "human.txt")
+
+human <- mutate(human,GNI.capita=str_replace(human$GNI.capita, pattern=",", replace ="") %>% as.numeric())
+
+keep <- c("country", "edu_2nd_ratio", "lab_ratio", "life_exp", "edu_exp", "GNI.capita", "mat_mort", "adol_birth", "parl_rep_f")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human)==TRUE)
+
+
+last <- nrow(human_) - 7
+last
+# choose everything until the last 7 observations
+human_ <- human_[1:last, ]
+
+# add countries as rownames
+rownames(human_) <- human_$country
+
+# remove the Country variable
+human_ <- select(human_, -country)
+
+write.table(human_, "human_.txt")
+
